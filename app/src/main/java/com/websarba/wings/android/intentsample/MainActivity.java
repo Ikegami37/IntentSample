2eaaab9323dfc5d45f4253aa335a983a35ee093a
Ikegami37 <92580968+Ikegami37@users.noreply.github.com>
2022-02-28 19:01:22 +0900

    Initial commit

---
+++ app/src/main/java/com/websarba/wings/android/intentsample/MainActivity.java
@@ -0,0 +1,102 @@
+package com.websarba.wings.android.intentsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        List<Map<String, String>> menuList = new ArrayList<>();
+
+        Map<String, String> menu = new HashMap<>();
+        menu.put("name", "唐揚げ定食");
+        menu.put("price", "800円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ハンバーグ定食");
+        menu.put("price", "850円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "生姜焼き定食");
+        menu.put("price", "900円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ステーキ定食");
+        menu.put("price", "1000円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "野菜炒め定食");
+        menu.put("price", "900円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "とんかつ定食");
+        menu.put("price", "1000円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ミンチカツ定食");
+        menu.put("price", "1000円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "チキンカツ定食");
+        menu.put("price", "900円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ラーメン定食");
+        menu.put("price", "800円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "回鍋肉定食");
+        menu.put("price", "800円");
+        menuList.add(menu);
+
+        String[] from = {"name", "price"};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, menuList,
+                android.R.layout.simple_list_item_2, from, to);
+        lvMenu.setAdapter(adapter);
+
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Map<String, String> item = (Map<String, String>) parent.getItemAtPosition(position);
+            String menuName = item.get("name");
+            String menuPrice = item.get("price");
+
+            Intent intent = new Intent(MainActivity.this, MenuThanksActivity.class);
+
+            intent.putExtra("menuName", menuName);
+            intent.putExtra("menuPrice", menuPrice);
+
+            startActivity(intent);
+        }
+    }
+
+}
+
+
